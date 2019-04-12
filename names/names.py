29deb220b43a01bce4f987f2715d6c74f420b05f
David Freitag <freitagdavid@outlook.com>
2019-04-12 16:28:42 -0400

    names/names: Working

--- names/names.py
+++ names/names.py
@@ -1,22 +1,33 @@
 import time
+from binsearch import BinarySearchTree
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+
+def bin_tree_from_file(file_name):
+    with open(file_name, 'r') as f:
+        working = f.read().split('\n')
+        bin_tree = BinarySearchTree()
+        for i in working:
+            bin_tree.insert(i)
+        return bin_tree
+
+
+names_1 = bin_tree_from_file('names_1.txt')
+names_2 = bin_tree_from_file('names_2.txt')
+
+print(len(names_1))
+
+
+def is_equal(x):
+    if names_2.contains(x):
+        duplicates.append(x)
+
+
+names_1.for_each(is_equal)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
